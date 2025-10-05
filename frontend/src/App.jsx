9f80f8d28ddcc58416cd5b7adbdc62166874abec
nabixka <nabixka05@gmail.com>
2025-10-05 21:19:06 +0700

    Landing Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,50 @@
-import { useNavigate } from "react-router-dom";
+import React from "react";
+
+function LoginButton() {
+  return (
+ 
+    <div className="flex justify-end p-3 sm:p-5"> 
+      <button 
+        className="bg-gray-700 text-white font-semibold py-2 px-3 text-sm rounded-lg shadow-lg hover:bg-gray-800 transition duration-300 transform hover:scale-105 md:text-base md:px-4"
+      >
+        Login Sebagai Pembina
+      </button>
+    </div>
+  );
+}
+
+function Isi(){
+  return(
+
+    <div className="flex flex-col items-center justify-center min-h-screen text-center p-6 sm:p-8 pt-20">
+        
+        <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-gray-900 mb-4 tracking-tight max-w-lg sm:max-w-3xl">
+          Sistem Informasi Ekstrakurikuler
+        </h1>
+
+        <p className="text-base sm:text-lg text-gray-600 mb-8 max-w-xs sm:max-w-2xl">
+          Temukan, kelola, dan ikuti kegiatan ekstrakurikuler sekolah Anda dengan mudah. Panduan terpadu untuk siswa dan pembina.
+        </p>
+
+        <div className="flex justify-center mt-6">
+          <button 
+            className="bg-blue-600 text-white text-md sm:text-lg font-bold py-3 px-6 sm:px-8 rounded-full shadow-xl hover:bg-blue-700 transition duration-300 transform hover:scale-105 sm:w-100 max-w-xs sm:max-w-none "
+          >
+            📥 Download Aplikasi 
+          </button>
+        </div>      
+      </div>
+  )
+}
 
 export default function App() {
-  const navigate = useNavigate();
-
-  const handleLoginClick = () => {
-    navigate("/login"); // pindah ke halaman login
-  };
-
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Selamat Datang di Website Saya</h1>
-      <p>Ini adalah landing page sederhana React.</p>
-      <button
-        onClick={handleLoginClick}
-        style={{
-          padding: "10px 20px",
-          backgroundColor: "#007bff",
-          color: "white",
-          border: "none",
-          borderRadius: "5px",
-          cursor: "pointer",
-        }}
-      >
-        Masuk ke Login
-      </button>
+    <div className="min-h-screen bg-white font-sans">
+      <header className="absolute top-0 w-full z-10">
+        <LoginButton />
+      </header>
+      <Isi />
+      
     </div>
   );
 }
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nome(){
+    return(
+        <h3>Hai</h3>
+    )
+}
+
+export default function Home(){
+    return(
+        <Nome />
+    )
+}
