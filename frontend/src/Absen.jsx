d8883754bf87244fd463c25577c4aed101faf79f
nabixka <nabixka05@gmail.com>
2025-10-14 10:13:50 +0700

    Add Animation

--- frontend/src/Absen.jsx
+++ frontend/src/Absen.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function Navbar(){
     const navigate = useNavigate();
@@ -18,14 +19,18 @@ function Navbar(){
     }
 
     return(
-    <div className="pt-5 flex justify-center">
+    <motion.div 
+    initial={{ y: -100 }}
+    animate={{ y: 0 }}
+    transition={{ duration: 1.4, ease: "easeOut"}}
+    className="pt-5 flex justify-center">
         <div className="bg-[rgba(243,244,246,0.2)] shadow-2xl rounded-full p-1 w-100 flex justify-between">
             <button onClick={home} className="text-xs sm:text-base bg-transparent rounded-full w-20">Home</button>
             <button onClick={jadwal} className="text-xs sm:text-base bg-transparent rounded-full">Buat Jadwal</button>
             <button onClick={dokum}className="text-xs sm:text-base bg-transparent rounded-full">Dokumentasi</button>
             <button pnClick={absensi} className="text-xs sm:text-base bg-gray-100 p-1 rounded-full w-20">Absensi</button>
         </div> 
-    </div>
+    </motion.div>
     )
 }
 
